59daee1c7b5e62ce4e8dc36c3944f995f94f9f3e
triplee12 <tripleeoliver@gmail.com>
2023-07-05 20:06:40 +0100

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,15 @@ interface Teacher{
 interface Directors extends Teacher{
     numberOfReports: number
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    const initial = firstName.charAt(0).toUpperCase();
+    const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+    return `${initial}. ${formattedLastName}`;
+};
