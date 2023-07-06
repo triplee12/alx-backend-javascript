3dac3c7d44794850bced8790baa61eb14d329d64
triplee12 <tripleeoliver@gmail.com>
2023-07-06 01:00:18 +0100

    Update task_4/js/main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,28 @@
+import { Subjects } from "./subjects/Subject";
+
+const { Cpp, Java, React } = Subjects;
+
+const cpp = new Cpp();
+const java = new Java();
+const react = new React();
+
+const cTeacher: Subjects.Teacher = {
+    firstName: "John",
+    lastName: "Doe",
+    experienceTeachingC: 10,
+};
+
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
