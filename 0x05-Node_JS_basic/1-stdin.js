70dc53199fa64e5574edfb5e8841d79a8f8abbbe
triplee12 <tripleeoliver@gmail.com>
2023-08-22 18:13:58 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?")
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write('Your name is: ' + chunk);
-        process.stdout.write("This important software is now closing\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+    const name = process.stdin.read();
+    if (name !== null) {
+        process.stdout.write(`Your name is: ${name}`);
     }
 });
-
-process.stdin.on('end', function() {
-    process.stdout.write('end');
+process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
 });
